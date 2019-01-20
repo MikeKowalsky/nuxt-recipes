c3aae0dd1cf6d12e6d2491862aa31efb61821a55
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-20 14:48:58 +0100

    added Header component to layout

---
+++ components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Starting page</nuxt-link></li>
+        <li><nuxt-link to="/recipes">Recipes</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  background-color: #521751;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
+
